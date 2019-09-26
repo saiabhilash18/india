d701175d40b04e7c14d8fe21a501506289a57fdf
saiabhilash18 <55742034+saiabhilash18@users.noreply.github.com>
2019-09-26 16:08:17 +0530

    Update EqualsExample.java

--- srilanka/src/srilanka/EqualsExample.java
+++ srilanka/src/srilanka/EqualsExample.java
@@ -6,8 +6,9 @@ public class EqualsExample {
 		String s2="javatpoint";  
 		String s3="JAVATPOINT";  
 		String s4="python";  
-		System.out.println(s1.equals(s2));//true because content and case is same  
-		System.out.println(s1.equals(s3));//false because case is not same  
-		System.out.println(s1.equals(s4));//false because content is not same  
+		System.out.println(s1.equals(s2)); 
+		System.out.println(s1.equals(s3));  
+		System.out.println(s1.equals(s4));
+		System.out.println("hello srilanka");
 		}
 }
