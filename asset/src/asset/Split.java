7234569e759b7713c7492feed312aa447960e7dc
saiabhilash18 <55742034+saiabhilash18@users.noreply.github.com>
2019-09-26 15:44:16 +0530

    initial commit
    fff

---
+++ asset/src/asset/Split.java
@@ -0,0 +1,16 @@
+package asset;
+
+public class Split {
+
+
+	public static void main(String[] args) {
+		String str=" welcome all  have a seat";
+		 String[] words=str.split(" ");
+		 for(String s:words){
+			 System.out.println(s);
+		 }
+System.out.println(str);
+
+	}
+
+}
